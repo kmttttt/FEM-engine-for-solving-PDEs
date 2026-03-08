0669de4b472908476c1bbe21579bfcb9a0cc1bc8
Kushagra Mani Tripathi <manitripathikushagra@gmail.com>
2026-03-08 19:52:18 +0530

    Global Stiffness and Mass matrix Assembly

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <Eigen/Sparse>
+#include "mesh_generator.h"
 
 int main() {
     // Define a sparse matrix of doubles
@@ -8,6 +9,8 @@ int main() {
     std::cout << "Successfully integrated Eigen!" << std::endl;
     std::cout << "Global Stiffness Matrix K dimensions: " 
               << K.rows() << " x " << K.cols() << std::endl;
-              
+    Mesh1D mesh(1.0, 0.0, 100);
+    auto uniform_mesh = mesh.generateUniform();
+    std::cout << "Generated uniform mesh with " << uniform_mesh.size() << " points." << std::endl;
     return 0;
 }
